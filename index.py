5824cc336b760833946fa94001ed31a1bb8ed76f
Rishab Lamba <rishablamba407@gmail.com>
2021-06-19 16:17:32 +0530

    UPDATE: Image support and hyperlink for messages added

--- index.py
+++ index.py
@@ -39,32 +39,39 @@ third_source_group = next(
 target_group = next(
     (chat for chat in chats if int(chat.id) == 596451463), None)
 
-
+        
 @client.on(events.NewMessage(source_group))
 async def main(event):
+    if event.photo:
+        link = 'https://t.me/c/'+str(1189021166)+'/'+str(event.message.id)+'\n**'+source_group.title+'**'
+        await client.send_file(596451463, event.media, caption=link)
     message = event.message.message
     if("delhi" in message.lower() or
         ("available" in message.lower() and "?" not in message) or
-            re.search("\b(D|d|ND) *(:|-)", message)):
-        await client.send_message(596451463, "**"+source_group.title+'**\n\n'+event.message.message)
-
+            re.search("(D|d|ND) *(:|-)", message)):
+        await client.send_message(596451463,'https://t.me/c/'+str(event.message.peer_id.channel_id)+'/'+str(event.message.id)+'\n**'+source_group.title+'**'+event.message.message)
 
 @client.on(events.NewMessage(second_source_group))
 async def main(event):
+    if event.photo:
+        link = 'https://t.me/c/'+str(1455095644)+'/'+str(event.message.id)+'\n**'+second_source_group.title+'**'
+        await client.send_file(596451463, event.media, caption=link)
     message = event.message.message
     if("delhi" in message.lower() or
         ("available" in message.lower() and "?" not in message) or
-            re.search("\b(D|d|ND) *(:|-)", message)):
-        await client.send_message(596451463, "**"+second_source_group.title+'**\n\n'+event.message.message)
-
+            re.search("(D|d|ND) *(:|-)", message)):
+        await client.send_message(596451463,'https://t.me/c/'+str(event.message.peer_id.channel_id)+'/'+str(event.message.id)+'\n**'+second_source_group.title+'**\n\n'+event.message.message)
 
 @client.on(events.NewMessage(third_source_group))
 async def main(event):
+    if event.photo:
+        link = 'https://t.me/c/'+str(1427274960)+'/'+str(event.message.id)+'\n**'+third_source_group.title+'**'
+        await client.send_file(596451463, event.media, caption=link)
     message = event.message.message
     if("delhi" in message.lower() or
         ("available" in message.lower() and "?" not in message) or
-            re.search("\b(D|d|ND) *(:|-)", message)):
-        await client.send_message(596451463, "**"+third_source_group.title+'**\n\n'+event.message.message)
+            re.search("(D|d|ND) *(:|-)", message)):
+        await client.send_message(596451463,'https://t.me/c/'+str(event.message.peer_id.channel_id)+'/'+str(event.message.id)+'\n**'+third_source_group.title+'**\n\n'+event.message.message)
 
 client.run_until_disconnected()
-print('Members scraped successfully.')
+print('Messages scraped successfully.')
